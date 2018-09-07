06e64afa3c5459a60ddb6cae1400da4b2ad8e760
juanpaz12 <42146514+juanpaz12@users.noreply.github.com>
2018-09-07 15:28:47 -0500

    :(

--- semana5/factorial.c
+++ semana5/factorial.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 int main ()
 {
-	int i,fac=1,n,op=1;
+	int i,n,op=1,j;
 	while(op==1){
-	
+	j=1;
 	printf("Por favor introduzca el numero el cual quiere calcular el factorial \n");
 	scanf("%i",&n);
 	for(i=1;i<=n;i++){
-		fac*=i;
-	}
-		printf("\nEl factorial del numero %i es %i\n",n,fac );
-		fac=1;
-printf("Desea volver a correr el programa? presione 1 para si y cualquiero otro numero para no\n");
-scanf("%i",&op);
+          
+		j=j*i;
+}
+        printf("\nEl factorial del numero %i es %i\n",n,j);
+        printf("Desea volver a correr el programa? presione 1 para si y cualquiero otro numero para no\n");
+        scanf("%i",&op);
 }
 return 0;
 } 
--- semana5/números_primos.c
+++ semana5/números_primos.c
@@ -5,18 +5,29 @@
 
 int main()
 {  
-   int i, inicial, final, n, par;
+   int i, inicial, final, raiz,m , primo;
    
-   printf("Hola con este programa puedes saber cuales son los numeros primos en un intevalo de numeros");
-   printf("Por favor ingrasa el numero minimo del intervalo a evaluar");
+   printf("Hola con este programa puedes saber cuales son los numeros primos en un intevalo de numeros\n");
+   printf("Por favor ingrasa el numero minimo del intervalo a evaluar\n");
    scanf("%i",&inicial);
-   printf("Por favor ingrasa el numero maximo del intervalo a evaluar");
+   printf("Por favor ingrasa el numero maximo del intervalo a evaluar\n");
    scanf("%i",&final);
    
-   for(i=inicial;i<=final;i++){
-   	if(i%2¡=0)
-   
+      while((final-inicial)>100)
+{
+         printf("\nel intervalo ingresado es muy grande, ingrasa valores mas pequeños: ");
+         scanf("%i %i", &inicial, &final);
 }
-return 0
+  
+     for(i=inicial;i<=final;i++)
+     {raiz=sqrt(i);
+     for(m=1;m<=raiz;m++){
+     if(i%m!=0){primo=0;
+        }
+if (primo==0){
+    printf("los numeros primeros de %i a %i son %i\n",inicial, final,i);}
+   } 
+}
+return 0;
 }
 
